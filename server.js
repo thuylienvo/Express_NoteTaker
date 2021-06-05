187ba2ca0fe8935077a18c13e8b369df6fd0af9a
Mimi Vo <mimzy414@gmail.com>
2021-06-05 12:26:30 -0500

    updated port

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const app = express();
-// const apiRoutes = require('./routes/apiRoutes');
+
+// JS files 
+const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Public file 
 app.use(express.static('public'));
 
 // Use apiRoutes
-// app.use('/api', apiRoutes);
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
