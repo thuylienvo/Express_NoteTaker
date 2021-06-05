f4825fdb04a5e212e3717a90a43abddac8264d13
Mimi Vo <mimzy414@gmail.com>
2021-06-05 18:21:06 -0500

    nothing works

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,29 +1,30 @@
 const router = require('express').Router();
 const fs = require('fs');
 const path = require('path');
-const { notes } = require('../db/db.json');
-
-
+let { notes } = require('../db/db.json');
 
 // PKG TO PULL IN UUID
 const { v4: uuidv4 } = require('uuid');
 
-// ROUTES 
-router.post('/notes', (req, res) => {
-    console.log('Proof this is somewhat working XP')
 
-    let results = notes;
-    if (req.query) {
-      results = filterByQuery(req.query, results);
-    }
-    res.json(results);
-  });
+//get access to db.json file
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
 
-//ADD A NOTE
+//post new notes
+router.post('/notes', function(req, res) {
+    req.body.id = uuidv4();
+
+    //use function to take in data to send
+    const newNote = addNewNote(req.body, notes);
+    res.json(newNote);
+});
+
+
+// DELETE REQUEST 
 
 
 
-
-//DELETE A NOTE
 
 module.exports = router;
