13cc10a84b68bde823df2061e81cb5d98af85968
Mimi Vo <mimzy414@gmail.com>
2021-06-05 17:41:49 -0500

    UUID installation

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const fs = require('fs');
+const path = require
+const { notes } = require('../db/db.json');
+
+
+
+// PKG TO PULL IN UUID
+const { v4: uuidv4 } = require('uuid');
+
+// ROUTES 
+router.post('/notes', (req, res) => {
+    console.log('Proof this is somewhat working XP')
+
+    let results = notes;
+    if (req.query) {
+      results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+  });
+
+//ADD A NOTE
+
+
+
+
+//DELETE A NOTE
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,30 @@
+// DEPENDENCIES
 const express = require('express');
-
 const PORT = process.env.PORT || 3000;
+
+// SET UP EXPRESS 
 const app = express();
 
 // JS files 
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+// PUBLIC FILES 
+app.use(express.static('public'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//Public file 
-app.use(express.static('public'));
-
-// Use apiRoutes
+// REQUIRED ROUTES 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+
+app.get('/api/notes', (req, res) => {
+    res.send('I hope this works!');
+  });
+
+// START SERVER 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   }); 
