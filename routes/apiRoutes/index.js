135561de30be67956f43bd4bc456893f97ccf0fc
Mimi Vo <mimzy414@gmail.com>
2021-06-05 21:42:34 -0500

    heroku fix

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,16 +1,16 @@
-// const router = require('express').Router();
-// const fs = require('fs');
-// const path = require('path');
-// let { notes } = require('../db/db.json');
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+let { notes } = require('./db/db.json');
 
 // PKG TO PULL IN UUID
 // const { v4: uuidv4 } = require('uuid');
 
 
 //get access to db.json file
-// router.get('/notes', (req, res) => {
-//     res.json(notes);
-// });
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
 
 //post new notes
 // router.post('/notes', function(req, res) {
